875fa64b27dad73df708193f960a7e1b3addfdef
Vishal Kaushik <vishalkaushik2002@gmail.com>
2024-09-21 17:12:06 +0530

    Updated detect_faces.py

--- detect_faces.py
+++ detect_faces.py
@@ -1,45 +1,49 @@
 ### To detect faces in images
-import numpy as np
+
 import argparse
 import cv2
+import numpy as np
 
-# To construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="path to input image")
-ap.add_argument("-p", "--prototxt", required=True, help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True, help="path to Caffe pre-trained model")
-ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probability to filter weak detections")
-args = vars(ap.parse_args())
 
-# load our serialized model from disk
-print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+def main():
 
-# load the input image and construct an input blob for the image
-# by resizing to a fixed 300x300 pixels and then normalizing it
-image = cv2.imread(args["image"])
-(h, w) = image.shape[:2]
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True, help="path to input image")
+    ap.add_argument("-p", "--prototxt", required=True, help="path to Caffe 'deploy' prototxt file")
+    ap.add_argument("-m", "--model", required=True, help="path to Caffe pre-trained model")
+    ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probability to filter weak detections")
+    args = vars(ap.parse_args())
 
-blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+    print("[INFO] loading model...")
+    net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
-# pass the blob through the network and obtain the detections and
-# predictions
-print("\n [INFO] computing object detections...")
-net.setInput(blob)
-detections = net.forward()
+    # load the input image and construct an input blob for the image
+    # by resizing to a fixed 300x300 pixels and then normalizing it
+    image = cv2.imread(args["image"])
+    (h, w) = image.shape[:2]
 
-# loop over the detections
-for i in range(0, detections.shape[2]):
-    confidence = detections[0, 0, i, 2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
 
-    if confidence > 0.5:
-        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-        (startX, startY, endX, endY) = box.astype("int")
+    # pass the blob through the network and obtain the detections and predictions
+    print("\n [INFO] computing object detections...")
+    net.setInput(blob)
+    detections = net.forward()
 
-        text = "{:.2f}%".format(confidence * 100)
-        y = startY - 10 if startY - 10 > 10 else startY + 10
-        cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
-        cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
 
-cv2.imshow("Output", image)
-cv2.waitKey(0)
+        if confidence > 0.5:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            text = "{:.2f}%".format(confidence * 100)
+            y = startY - 10 if startY - 10 > 10 else startY + 10
+            cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+            cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+    cv2.imshow("Output", image)
+    cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+    main()
