f3c0133254cc4476c90ec5442de29fb93253a7c0
Vishal Kaushik <vishalkaushik2002@gmail.com>
2024-09-24 20:53:30 +0530

    Updated facial_detection.py

--- facial_detection.py
+++ facial_detection.py
@@ -60,7 +60,7 @@ def main():
 		cv2.imshow("Frame", frame)
 		key = cv2.waitKey(1) & 0xFF
 
-		# if the `q` key was pressed, break from the loop
+		# if the `q` key is pressed, break from the loop
 		if key == ord("q"):
 			break
 
